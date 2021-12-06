dcb897ec70aa62e58f348f9770ccf5982a940523
Long Nguyen <nguyenhuulong312@gmail.com>
2021-12-06 15:26:26 +0700

    Add client src first time

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,36 +1,107 @@
-import React, { Component } from 'react';
+import React, { useEffect, Fragment, Suspense, lazy } from 'react';
+import { Switch, Route, useHistory } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { useTransition } from 'react-spring';
 
-class App extends Component {
-state = {
-    data: null
-  };
+import { selectCurrentUser } from '../../redux/user/userSelectors';
+import { signInStart } from '../../redux/user/userActions';
+import { connectSocket } from '../../redux/socket/socketActions';
+import { fetchNotificationsStart } from '../../redux/notification/notificationActions';
 
-  componentDidMount() {
-    this.callBackendAPI()
-      .then(res => this.setState({ data: res.express }))
-      .catch(err => console.log(err));
-  }
-    // fetching the GET route from the Express server which matches the GET route from server.js
-  callBackendAPI = async () => {
-    const response = await fetch('/express_backend');
-    const body = await response.json();
+import ProtectedRoute from '../ProtectedRoute/ProtectedRoute';
+import Header from '../Header/Header';
+import Alert from '../../components/Alert/Alert';
+import Footer from '../../components/Footer/Footer';
+import LoadingPage from '../../pages/LoadingPage/LoadingPage';
 
-    if (response.status !== 200) {
-      throw Error(body.message) 
+const LoginPage = lazy(() => import('../../pages/LoginPage/LoginPage'));
+const SignUpPage = lazy(() => import('../../pages/SignUpPage/SignUpPage'));
+const HomePage = lazy(() => import('../../pages/HomePage/HomePage'));
+
+export function UnconnectedApp({
+  signInStart,
+  alert,
+  currentUser,
+  connectSocket,
+  fetchNotificationsStart,
+}) {
+  const token = localStorage.getItem('token');
+  const {
+    location: { pathname },
+  } = useHistory();
+
+  useEffect(() => {
+    if (token) {
+      signInStart(null, null, token);
+      connectSocket();
+      fetchNotificationsStart(token);
     }
-    return body;
+  }, [signInStart, connectSocket, fetchNotificationsStart, token]);
+
+  const transitions = useTransition(alert.showAlert, null, {
+    from: {
+      transform: 'translateY(4rem)',
+    },
+    enter: {
+      transform: 'translateY(0rem)',
+    },
+    leave: {
+      transform: 'translateY(4rem)',
+    },
+    config: {
+      tension: 500,
+      friction: 50,
+    },
+  });
+
+  const renderApp = () => {
+    // Wait for authentication
+    if (!currentUser && token) {
+      return <LoadingPage />;
+    }
+    return (
+      <Fragment>
+        {pathname !== '/login' && pathname !== '/signup' && <Header />}
+        {transitions.map(
+          ({ item, props, key }) =>
+            item && (
+              <Alert key={key} style={props} onClick={alert.onClick}>
+                {alert.text}
+              </Alert>
+            )
+        )}
+        <Switch>
+          <Route path="/login" component={LoginPage} />
+          <Route path="/signup" component={SignUpPage} />
+          <ProtectedRoute exact path="/" component={HomePage} />
+        </Switch>
+        {pathname !== '/' && <Footer />}
+        {pathname !== '/login' &&
+          pathname !== '/signup' &&
+          pathname !== '/new' &&
+          currentUser}
+      </Fragment>
+    );
   };
 
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">{this.state.data}</p>
-      </div>
-    );
-  }
+  return (
+    <div className="app" data-test="component-app">
+      <Suspense fallback={<LoadingPage />}>{renderApp()}</Suspense>
+    </div>
+  );
 }
 
-export default App;
+const mapStateToProps = (state) => ({
+  modal: state.modal,
+  alert: state.alert,
+  currentUser: selectCurrentUser(state),
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  signInStart: (usernameOrEmail, password, token) =>
+    dispatch(signInStart(usernameOrEmail, password, token)),
+  connectSocket: () => dispatch(connectSocket()),
+  fetchNotificationsStart: (authToken) =>
+    dispatch(fetchNotificationsStart(authToken)),
+});
+export default connect(mapStateToProps, mapDispatchToProps)(UnconnectedApp);
